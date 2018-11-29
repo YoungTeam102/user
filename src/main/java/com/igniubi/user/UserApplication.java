75897fed5bf66e006f8bcefce1c82658c4429e4e
shuai.zhang <shuai.zhang@yoho.cn>
2018-11-29 20:59:20 +0800

    整理依赖

--- src/main/java/com/igniubi/user/UserApplication.java
+++ src/main/java/com/igniubi/user/UserApplication.java
@@ -3,11 +3,13 @@ package com.igniubi.user;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 @MapperScan("com.igniubi.user.dao")
 @ComponentScan(basePackages = "com.igniubi")
 public class UserApplication {
